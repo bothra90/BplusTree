2a17139a905c71f0bf2b6e2177fdabefc88fecfa
Prashant <prashant@prashant-laptop.(none)>
2012-02-09 04:46:08 +0530

    look-up code done. to be debugged after insertion code

--- btree.cpp
+++ btree.cpp
@@ -1,4 +1,138 @@
-#include<btree.hpp>
+#include <btree.hpp>
+#include <fstream>
+#include <cmath> 
 
-int main(){
+Index::Index(string _indexname, KeyType * _keytype, int _payloadlen){
+  indexname = _indexname;
+  keytype = _keytype;
+  payloadlen = _payloadlen;
+  file.open(indexname.c_str(), ios::in|ios::binary|ios::ate | ios::out);
+  maxKeys = (BLOCKSIZE - 1 - sizeof(int) - fmax(sizeof(payloadlen), 8)) / ( + keylen(KeyType) + fmax(sizeof(payloadlen), 8));
+  // Store keytype in first block
+  file << keytype -> numAttrs;
+  for(int i = 0;i < keytype -> numAttrs;i++){
+    file << keytype -> attrTypes[i];
+  }
+  for(int i = 0;i < keytype -> numAttrs;i++){
+    file << keytype -> attrLen[i];
+  }
+  file << payloadlen;
+  root = new node;
+  file << (long long int)0; // Initial offset of NULL for root, i.e., no root yet.
+  root -> offset = 0;
+}
+
+node * Index::getRoot(){
+  long long int offset;
+  file.seekg(sizeof(int) + keytype -> numAttrs * ( sizeof(int) + sizeof(attrType)) + sizeof(int));
+  file >> offset;
+  return fetchNode(offset);
+}
+
+node * Index::fetchNode(long long int offset){
+  file.seekg(offset);
+  node * ret = new node;
+  ret -> offset = offset;
+  file >> ret -> isLeaf;
+  file >> ret -> numKeys;
+  ret -> keys = new char[ret -> numKeys * keylen(keytype)];
+  ret -> data = new char[ret -> (numKeys + 1) * fmax(payloadlen, 8)];
+  for(int i = 0;i < ret -> numKeys * keylen(keytype);i++){
+    file >> ret -> keys[i];
+  }
+  for(int i = 0;i < (ret -> numKeys + 1)* fmax(payloadlen, 8);i++){
+    file >> ret -> data[i];
+  }
+  return ret;
+}
+
+Index::Index(string _indexname){
+  indexname = _indexname;
+  file.open (indexname.c_str(), ios::in|ios::binary|ios::ate | ios::out);
+  keytype = new KeyType;
+  if (file.is_open()){
+    file >> keytype->numAttrs;
+    for(int i = 0;i < keytype->numAttrs;i++){
+      file >> keytype->attrTypes[i];
+    }
+    for(int i = 0;i < keytype->numAttrs;i++){
+      file >> keytype->attrLen[i];
+    }
+    file >> payloadlen;
+    // Set maxKeys
+    maxKeys = (BLOCKSIZE - 1 - sizeof(int) - fmax(sizeof(payloadlen), 8)) / ( + keylen(KeyType) + fmax(sizeof(payloadlen), 8));
+    // Load root node
+    root = getRoot();
+  }
+  else{
+    printf("Unable to open file\n");
+  }
+  file.close();
+}
+
+int Index::insert(byte key[], byte payload[]){
+}
+
+#define KEY(a) (a * keylen(keytype))
+
+/*Finds pointer to follow in a node using binary search on key*/
+int Index::find_ptr(node * n, byte key[]){
+  int left = 0, right = n -> numKeys - 1, current = n -> numKeys / 2;
+  byte * key1,* currKey ;
+  key1 = &(n -> keys[KEY(right)]);
+  if(compareKeys(key, key1) > 0) return right + 1;
+  while(left < right){
+    current = (left + right) / 2;
+    currKey = &(n -> keys[KEY(current)]);
+    if(compareKeys(key, currKey) > 0){
+      left = current;
+    }
+    else{
+      right = current;
+    }
+  }
+  return left + 1;
+}
+
+/*Finds key in a leaf node using binary search*/
+int Index::find_key(node * n, byte key[]){
+  assert(n -> isLeaf);
+  int left = 0, right = n -> numKeys - 1, current = n -> numKeys / 2;
+  byte * key1,* currKey ;
+  
+  while(left < right){
+    current = (left + right) / 2;
+    currKey = &(n -> keys[KEY(current)]);
+    if(compareKeys(key, currKey) > 0){
+      left = current;
+    }
+    else{
+      right = current;
+    }
+  }
+  current = (left + right) / 2;
+  currKey = &(n -> keys[KEY(current)]);
+  if(compareKeys(key, currKey) == 0){
+    return current;
+  }
+  return -1;
+}
+
+int Index::lookup(byte key[], byte payload[]){
+  if(root -> offset == NULL) return 0;
+  node * curr = root;
+  while(!curr -> isLeaf){
+    curr = curr -> data[find_ptr(curr, key) * OFFSETSIZE];
+  }
+  // assert(curr -> isLeaf);
+  int loc = find_key(curr, key);
+  if(loc == -1)
+    return 0;
+  else{
+    memcpy(payload, &(curr -> data[loc * payloadlen]), payloadlen);
+    for(int i = 0;i < payloadlen;i++){
+      payload[i] = curr -> data[loc * payloadlen + i];
+    }
+    return 1;
+  }
 }
--- btree.hpp
+++ btree.hpp
@@ -7,19 +7,31 @@
 #include<cstdio>
 #include<iostream>
 
-#define MaxAttrs 16
-#define BLOCKSIZE 100
 #define INTSIZE 4
-
+#define MAXATTRS 16
+#define BLOCKSIZE 4096
+#define OFFSETSIZE 8
 typedef byte char;
 
 enum attrType{intType, stringType};
 
 typedef struct KeyType_t{
   int numAttrs;
-  attrType attrTypes[MaxAttrs];
-  int attrLen[MaxAttrs];
+  attrType attrTypes[MAXATTRS];
+  int attrLen[MAXATTRS];
 } KeyType;
+
+typedef char byte;
+
+typedef struct node_t{
+  node * parent;
+  long long int offset; // offset == 0 => node does not exist
+  /*Following stored in index */   
+  bool isLeaf;
+  int numKeys;
+  byte * keys;
+  byte * data; // Payload / Block Pointers(Offsets)
+} node;
 
 int keylen		(KeyType *keytype);
 int setIntAttrval	(byte *key, KeyType * keytype, int attrnum, int val);
@@ -29,30 +41,35 @@ int getStringAttrVal	(byte *key, KeyType * keytype, int attrnum, char *retval);
 
 class Index{
 
-  private:
-    KeyType * keytype;
-    int payloadlen;
-    string indexname;
+private:
+  /* Index Variables*/ 
+  KeyType * keytype;
+  int payloadlen;
+  string indexname;
+  fstream file;
+  int maxKeys;
+  node * root;
 
-  public:
-    /*
-       It constructs an index in a file whose name is specified in indexname. You can assume a fixed directory for all files. The keytype and
-       payloadlen information, along with a pointer to the file offset of the root block of the tree, should be stored as a struct copied into the head
-       of the index. (Note that KeyType is of fixed length.)
-       */   
-    Index(string indexname, KeyType * keytype, int payloadlen);
-    /*
-       Opens an existing index, and retrieves the associated type/length information from the head of the index.  
-       */
-    Index(string indexname);
-    /*
-       Key comparison should be based on the type of the key specified earlier, and it should be easy to modify your code to add new types. You do
-       NOT have to make this object oriented, you just need a switch on the key type insider your insert/lookup functions.  The index should only
-       support non-duplicate keys. If a key is duplicated, insert should return 0, and not insert the duplicate. Otherwise a value of 1 is returned. */ 
-    int insert(byte key[], byte payload[]);
-    /*
-       Payload should be an array of bytes of the required size in which
-       the retrieved payload is stored. The return value is 1 if the value is found, and 0 otherwise. */ 
-    int lookup(byte key[], byte payload[]); 
+  /*Helper Functions*/   
+  node * getRoot();
+  node * fetchNode(long long int offset);
+
+  
+public:
+  /*
+    It constructs an index in a file whose name is specified in indexname. You can assume a fixed directory for all files. The keytype and payloadlen information, along with a pointer to the file offset of the root block of the tree, should be stored as a struct copied into the head of the index. (Note that KeyType is of fixed length.)
+  */   
+  Index(string _indexname, KeyType * _keytype, int _payloadlen);
+  /*
+    Opens an existing index, and retrieves the associated type/length information from the head of the index.  
+   */
+  Index(string _indexname);
+  /*
+    Key comparison should be based on the type of the key specified earlier, and it should be easy to modify your code to add new types. You do NOT have to make this object oriented, you just need a switch on the key type insider your insert/lookup functions.  The index should only support non-duplicate keys. If a key is duplicated, insert should return 0, and not insert the duplicate. Otherwise a value of 1 is returned. */ 
+  int insert(byte key[], byte payload[]);
+  /*
+    Payload should be an array of bytes of the required size in which
+the retrieved payload is stored. The return value is 1 if the value is found, and 0 otherwise. */ 
+  int lookup(byte key[], byte payload[]); 
 };
 #endif
