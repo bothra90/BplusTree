a1a355c7c1b2d25dcbc0b4bc999fd952e6237b02
Abhay <bothra90@gmail.com>
2012-02-09 17:24:17 +0530

    test case added

--- testcase.cpp
+++ testcase.cpp
@@ -1,6 +1,16 @@
 #include <fstream>
 #include <btree.hpp>
 
+void insert_key(byte * key, int a, int b, char c[8]){
+  int offset = 0;
+  memcpy(key + offset, a, INTSIZE);
+  offset += 4;
+  memcpy(key + offset, c, 8);
+  offset += 8;
+  memcpy(key + offset, b, INTSIZE);
+  return;
+}
+
 
 int main(){
   key = new KeyType;
@@ -15,4 +25,10 @@ int main(){
   key->attrLen[1] = 8; // 8 character string
   int payloadLen = 1;
   Index foo = new Index("foo",key, payloadLen);
+  int keylen = keyLength(key);
+  byte key[] = new byte[keylen];
+  insert_key(key, 4, 12, "this is ");
+  foo.insert(key,'a');
+  byte * res= new byte;
+  foo.lookup(key,res);
 }
